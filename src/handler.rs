cbdf33433deaefecff79ccd66e0d3673e3104a56
ashestosea <ashestosea@gmail.com>
2023-06-20 13:25:43 -0500

    Add input for moving to prev/next date

--- src/handler.rs
+++ src/handler.rs
@@ -1,5 +1,5 @@
-use crate::app::{App, InputMode, AppResult};
-use crossterm::event::{Event, KeyCode, KeyEvent};
+use crate::app::{App, AppResult, InputMode};
+use crossterm::event::{Event as CrosstermEvent, KeyCode, KeyEvent, KeyModifiers};
 use tui_input::backend::crossterm::EventHandler;
 
 pub fn handle_key_events(mut app: &mut App, key: KeyEvent) -> AppResult<()> {
@@ -13,19 +13,41 @@ pub fn handle_key_events(mut app: &mut App, key: KeyEvent) -> AppResult<()> {
             }
             _ => {}
         },
-        InputMode::Logging => match key.code {
-            KeyCode::Enter => {
-                app.add_entry(app.input.value().into());
-                app.input.reset();
-            }
-            KeyCode::Esc => {
-                app.input_mode = InputMode::Editing;
-            }
-            _ => {
-                app.input.handle_event(&Event::Key(key));
-            }
+        InputMode::Logging => match key.modifiers {
+            KeyModifiers::CONTROL => match key.code {
+                KeyCode::Left => match app.move_prev_day() {
+                    Ok(_) => {}
+                    Err(e) => {
+                        println!("{}", e)
+                    }
+                },
+                KeyCode::Right => match app.move_next_day() {
+                    Ok(_) => {}
+                    Err(e) => {
+                        println!("{}", e)
+                    }
+                },
+                _ => {}
+            },
+            _ => match key.code {
+                KeyCode::Enter => {
+                    app.add_entry(app.input.value().into());
+                    app.input.reset();
+                }
+                KeyCode::Esc => {
+                    app.input_mode = InputMode::Editing;
+                }
+                _ => {
+                    app.input.handle_event(&CrosstermEvent::Key(KeyEvent {
+                        code: key.code,
+                        modifiers: key.modifiers,
+                        kind: key.kind,
+                        state: key.state,
+                    }));
+                }
+            },
         },
     }
-    
+
     Ok(())
 }
