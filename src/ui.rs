110451a876f7dea1522060885e0d2f899a756209
ashestosea <ashestosea@gmail.com>
2023-08-31 16:27:34 -0500

    Cleanup UI

--- src/ui.rs
+++ src/ui.rs
@@ -7,7 +7,7 @@ use crate::app::{App, InputMode};
 pub fn render<B: Backend>(app: &mut App, frame: &mut Frame<'_, B>) {
     let entry_group = &app.current_entries;
 
-    let top_layout = Layout::default()
+    let root_layout = Layout::default()
         .direction(Direction::Vertical)
         .margin(1)
         .constraints([Constraint::Percentage(100)].as_ref())
@@ -15,7 +15,7 @@ pub fn render<B: Backend>(app: &mut App, frame: &mut Frame<'_, B>) {
 
     let main_layout = Layout::default()
         .direction(Direction::Vertical)
-        .margin(1)
+        .horizontal_margin(1)
         .constraints(
             [
                 Constraint::Max(1),
@@ -26,15 +26,13 @@ pub fn render<B: Backend>(app: &mut App, frame: &mut Frame<'_, B>) {
             ]
             .as_ref(),
         )
-        .split(top_layout[0]);
-
-    let top_block = Block::default()
-        .borders(Borders::NONE)
-        // .border_style(Style::default())
-        // .border_type(BorderType::Rounded)
-        // .title("lipu-tenpo")
-        .title_alignment(Alignment::Center);
-    frame.render_widget(top_block, top_layout[0]);
+        .split(root_layout[0]);
+    
+    let date_area = main_layout[0];
+    let log_area = main_layout[1];
+    let summary_area = main_layout[2];
+    let input_area = main_layout[3];
+    let hotkeys_area = main_layout[4];
 
     // Date
     let current_date = app.current_date;
@@ -55,7 +53,7 @@ pub fn render<B: Backend>(app: &mut App, frame: &mut Frame<'_, B>) {
             &current_date.format("%Y─%m─%d")
         ))
         .style(date_style);
-    frame.render_widget(title_block, main_layout[0]);
+    frame.render_widget(title_block, date_area);
 
     // Log
     let log_block = Block::default()
@@ -77,14 +75,14 @@ pub fn render<B: Backend>(app: &mut App, frame: &mut Frame<'_, B>) {
         items.push(ListItem::new(Text::raw(time_since_last)));
     }
     let list: List = List::new(items).block(log_block);
-    frame.render_widget(list, main_layout[1]);
+    frame.render_widget(list, log_area);
 
     // Summary
     let summary_layout = Layout::default()
         .direction(Direction::Horizontal)
         .margin(0)
         .constraints([Constraint::Percentage(50), Constraint::Percentage(50)].as_ref())
-        .split(main_layout[2]);
+        .split(summary_area);
 
     let work_summary_block = Block::default()
         .borders(Borders::BOTTOM | Borders::LEFT)
@@ -110,7 +108,7 @@ pub fn render<B: Backend>(app: &mut App, frame: &mut Frame<'_, B>) {
     frame.render_widget(other_summary, summary_layout[1]);
 
     // Input
-    let width = main_layout[3].width.max(3) - 3;
+    let width = input_area.width.max(3) - 3;
     let scroll = app.input.visual_scroll(width as usize);
     let input_block = Block::default()
         .padding(Padding::horizontal(1))
@@ -119,19 +117,19 @@ pub fn render<B: Backend>(app: &mut App, frame: &mut Frame<'_, B>) {
     let input = Paragraph::new(app.input.value())
         .scroll((0, scroll as u16))
         .block(input_block);
-    frame.render_widget(input, main_layout[3]);
+    frame.render_widget(input, input_area);
 
     match app.input_mode {
         InputMode::Editing => {}
         InputMode::Logging => frame.set_cursor(
-            main_layout[3].x + 2 + ((app.input.visual_cursor()).max(scroll) - scroll) as u16,
-            main_layout[3].y + 1,
+            input_area.x + 2 + ((app.input.visual_cursor()).max(scroll) - scroll) as u16,
+            input_area.y + 1,
         ),
     }
 
     // Hot Keys
-    let hot_keys_block = Block::default().padding(Padding::horizontal(1)).dark_gray();
-    let hot_keys_help =
-        Paragraph::new("Ctrl+Left/Right: Prev/Next day, Ctrl+Home: Today").block(hot_keys_block);
-    frame.render_widget(hot_keys_help, main_layout[4]);
+    let hotkeys_block = Block::default().padding(Padding::horizontal(1)).dark_gray();
+    let hotkeys_help =
+        Paragraph::new("Ctrl+Left/Right: Prev/Next day, Ctrl+Home: Today").block(hotkeys_block);
+    frame.render_widget(hotkeys_help, hotkeys_area);
 }
